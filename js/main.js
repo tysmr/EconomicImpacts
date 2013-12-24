8f7bb300e3cd92fc3f4525fddcc8611955b2eab6
zebzebzeb <less_is_more_for_a_big_hope@yahoo.co.jp>
2013-12-25 00:18:36 +0900

    Deleted some script

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 
 
-// var formatPercent = d3.format(".0%");
-
-// tipのせる系 => 
-// posi nega => http://bl.ocks.org/mbostock/2368837
-
-// var margin = {top: 30, right: 100, bottom: 10, left: 100},
-    // width = 960 - margin.left - margin.right,
-    // height = 700 - margin.top - margin.bottom;
-
-// var x = d3.scale.linear()
-//     .range([0, width]);
-
-// var y = d3.scale.ordinal()
-//     .rangeRoundBands([0, height], .2);
-
-// var xAxis = d3.svg.axis()
-//     .scale(x)
-//     .orient("top");
-
-// var yAxis = d3.svg.axis()
-//     .scale(y)
-//     .orient("left")
-//     // .tickFormat(formatPercent);
-
 var tip = d3.tip()
     .attr('class', 'd3-tip')
     .offset([-10, 0])
@@ -31,18 +7,6 @@ var tip = d3.tip()
       return "<strong>金額:</strong> <span style='color:red'>" + d.value + "</span><sstrong>円</strong>";
     })
 
-// var svg = d3.select("div#content").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//     .append("g")
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// var yAxisStartPoint = [];
-
-// svg.call(tip);
-
-// initLoadData("/data/tokyo_5degree.csv");
-// x.domain(d3.extent(-100000, 200000));
 
 function changeCityData(data, city){
   	var city_margin = {top: 30, right: 10, bottom: 10, left: 10};
@@ -162,21 +126,8 @@ function changeData(data){
 
   svg.append("g")
       .attr("class", "y axis")
-      // .attr("x", yAxisStartPoint)
       .attr("transform", "translate("+yAxisStartPoint[0]+",0)")
-      // .attr("transform", function(d){
-      //   var xxx;
-      //   if(d){
-
-      //   }
-      //   var str = "transform("+  +",0)");
-      //   return str;
-      // })
       .call(yAxis)
-    // .append("line")
-    //   .attr("x1", x(0))
-    //   .attr("x2", x(0))
-    //   .attr("y2", height)
       .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
@@ -198,13 +149,6 @@ $( 'input[name="gorin"]:radio' ).change( function() {
   changeData('/data/'+$('input[name="gorin"]:checked').val()+'.csv');
   
 });
-// $('circle').on('mouseover', function() {
-//   // window.alert('impact of'+ $(this).attr('id'));
-//   $('g.x').empty();
-//   $('g.y').empty();
-//   console.log($(this).attr('id'));
-//   changeData('/data/'+$(this).attr('id')+'.csv');
-// });
 
 $('div#tokyo-svg').on('click', function() {
   console.log($(this).attr('id'));
